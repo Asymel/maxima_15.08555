30f86bbfab1711f80066dd694f4ed0b32132145b
- <romkakorshunob@gmail.com>
2024-10-16 15:29:35 +0300

    НМ

---
+++ src/lesson_6/Main.java
@@ -0,0 +1,18 @@
+package lesson_6;
+
+public class Main {
+    public static void main(String[] args) {
+//        int [] arr = new int[10];
+
+
+        String s = new String();
+        s = "Hello World";
+        System.out.println(s);
+
+        String a = "This is my first String";
+        System.out.println(a);
+
+    }
+
+
+}
